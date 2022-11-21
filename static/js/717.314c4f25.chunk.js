d249919fdce7495530d19f21c0b96977845cb051
Marianna1501 <109352232+Marianna1501@users.noreply.github.com>
2022-11-21 17:39:57 +0000

    Deploying to gh-pages from @ Marianna1501/goit-react-hw-08-phonebook@ca2f02b026d6dc34d29073426aea50daa9b25c1b 🚀

---
+++ static/js/717.314c4f25.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[717],{717:function(n,o,t){t.r(o);var c=t(184);o.default=function(){return(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)("h2",{children:"Save your contacts"}),(0,c.jsx)("img",{alt:"book",src:"https://cdn-icons-png.flaticon.com/512/1195/1195412.png"})]})}}}]);
+//# sourceMappingURL=717.314c4f25.chunk.js.map
---
+++ static/js/731.c99b96b2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[731],{731:function(e,o,n){n.r(o),n.d(o,{default:function(){return u}});var t=n(3504),r=n(184);function u(){return(0,r.jsxs)("h1",{children:["404 Page Not Found, go to ",(0,r.jsx)(t.rU,{to:"/",children:"Home Page"})]})}}}]);
+//# sourceMappingURL=731.c99b96b2.chunk.js.map
